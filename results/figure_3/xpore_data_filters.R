b081dfadededcf932125de5fa86f53b859a5ba2d
samir-watson <63302622+samir-watson@users.noreply.github.com>
2024-06-07 17:40:18 +0200

    Update and rename xpore data filters to xpore_data_filters.R

--- results/figure_3/xpore_data_filters.R
+++ results/figure_3/xpore_data_filters.R
@@ -1,6 +1,8 @@
 library(tidyverse)
+library(wesanderson)
 
-basedir <- ("C:/Users/au656873/OneDrive - Aarhus Universitet/Desktop/METTL3/xpore/all_KO")
+
+basedir <- ("C:/Users/au656873/OneDrive - Aarhus Universitet/Desktop/METTL3/xpore/A56")
 setwd(basedir)
 dir()
 
@@ -19,18 +21,18 @@ data <- read_csv("majority_direction_kmer_diffmod.table", col_names=TRUE)
 
 # Filter the data
 filtered_data <- data %>%
-  filter(pval_WT_vs_xKO < 0.01, z_score_WT_vs_xKO > 0) %>%
+  filter(pval_WT_vs_xKO.A56 < 0.05, z_score_WT_vs_xKO.A56 > 0) %>%
   filter(kmer %in% DRACH_motifs$DRACH)
 
 # Save the filtered data to a new TSV filec
-write_csv(filtered_data, "all_KO_filtered_majority_direction_kmer_diffmod.table")
+write_csv(filtered_data, "A56_filtered_majority_direction_kmer_diffmod.table")
 
 # Print a message to let the user know the file has been saved
 print("The filtered data has been saved to 'all_KO_filtered_majority_direction_kmer_diffmod.table'.")
 
 
-# Re3ad the TSV file
-data3 <- read_csv("all_KO_filtered_majority_direction_kmer_diffmod.table")
+# Read the TSV file
+data3 <- read_csv("A56_filtered_majority_direction_kmer_diffmod.table")
 
 data_char <- data3 %>%
   mutate_all(as.character)
@@ -40,7 +42,7 @@ data_no_na <- data_char %>%
   mutate_all(~replace_na(., ""))
 
 # Write the modified data to a new CSV file
-write_csv(data_no_na, "2all_KO_filtered_majority_direction_kmer_diffmod.table")
+write_csv(data_no_na, "A56_filtered_majority_direction_kmer_diffmod.table")
 
 # Print a message to let the user know the file has been saved
 print("The modified data has been saved to 'modified_file.csv'.")
